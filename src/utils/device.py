5780683786ce63c3be0a329560cac5384d8be30a
lixinzhen <lixinzhen2023@163.com>
2026-03-05 19:23:22 +0800

    fix(src/utils/device.py): implement proper order in device.py

--- src/utils/device.py
+++ src/utils/device.py
@@ -7,22 +7,32 @@ def pick_device(prefer = ("xpu", "cuda", "cpu"), device_cfg: str = "auto"):
     """
     import torch
 
+    # not auto
     if device_cfg != "auto":
-        return device_cfg
-
-    # XPU
-    try:
-        if hasattr(torch, "xpu") and torch.xpu.is_available():
-            return "xpu"
-    except Exception:
-        pass
-    
-    # CUDA
-    try:
-        if torch.cuda.is_available():
+        if device_cfg == "xpu":
+            try:
+                if hasattr(torch, "xpu") and torch.xpu.is_available():
+                    return "xpu"
+            except Exception:
+                pass
+        if device_cfg == "cuda" and torch.cuda.is_available():
             return "cuda"
-    except Exception:
-        pass
+        if device_cfg == "cpu":
+            return "cpu"
+        return "cpu"
 
-    # CPU
+    # auto
+    for dev in prefer:
+        if dev == "xpu":
+            try:
+                if hasattr(torch, "xpu") and torch.xpu.is_available():
+                    return "xpu"
+            except Exception:
+                pass
+        elif dev == "cuda" and torch.cuda.is_available():
+            return "cuda"
+        elif dev == "cpu":
+            return "cpu"
+
     return "cpu"
+
